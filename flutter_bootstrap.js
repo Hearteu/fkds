260eed2e0e9cf3aac3976b98d93c956d2ad3a047
Hearteu <Hearteu@users.noreply.github.com>
2025-09-30 01:01:36 +0000

    deploy: 50ca19724459cd4f66c66ff41d1f057bc421cef1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3737450188"
+    serviceWorkerVersion: "3221568293"
   }
 });
